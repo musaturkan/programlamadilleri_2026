d1ce70270e0bf35739550c4436fc307c6d7092c6
musaturkan <musa.turkan@bilecik.edu.tr>
2026-03-18 11:08:00 +0300

    aaa

---
+++ Services/Fabrika/IService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Fabrika
+{
+    public interface IService
+    {
+        void Yazdir(int id = 1);
+        //void Yazdir(DataModel.Urun urun);
+    }
+}
--- Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
+++ Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34fc180994b272048228755ae146a9824b63ca56")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75d115b865ee2350fa05b1366c97bcc0ac2f08e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
