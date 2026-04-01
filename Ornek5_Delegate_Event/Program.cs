9f82bc24e31c275299e34f2bfa9bbd461d829462
musaturkan <musa.turkan@bilecik.edu.tr>
2026-04-01 11:39:57 +0300

    delegate event örnekleri  eklendi

---
+++ Ornek5_Delegate_Event/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    delegate void temsilci();
+    static void Selamlama()
+    {
+        Console.WriteLine("Merhaba, Dünya!");
+    }   
+    
+    static void Yazdir() 
+    {         
+        Console.WriteLine("Bu bir delegate örneğidir.");
+    }
+    private static void Main(string[] args)
+    {
+        temsilci a;
+        a = Selamlama;
+        a();
+
+        a = Yazdir;
+        a();
+
+
+    }
+}
---
+++ Ornek5_Delegate_Event_Library/Arac.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ornek5_Delegate_Event_Library
+{
+    public class Arac
+    {
+        public string Marka { get; set; }
+        public string Model { get; set; }
+        public int Yil { get; set; }
+       
+        public void HareketEt(int km)
+        {
+           Console.WriteLine($"{Marka} {Model} Hareket etti.");
+        }
+
+        public void YakitEkle(int litre)
+        {
+            Console.WriteLine($"{litre} litre yakıt eklendi.");
+        }
+
+        ///Bir metoda paramater olarak başka bir metot verilebilir. 
+        ///Bu sayede o metot çalıştırıldığında, parametre olarak verilen metot da çalıştırılır.
+        ///Bu işlemi gerçekleştirmek için C# dilinde delegate yapısı kullanılır.
+        ///Delegate: Metot referanslarını tutan bir türdür. 
+        ///Bir delegate, belirli bir imzaya sahip metotları temsil eder ve bu metotları çağırmak için kullanılabilir.
+
+        delegate void HareketDelegate(int km);
+    }
+}
